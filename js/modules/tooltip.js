b3cc39553af75a1a958444bfdd008258062265fd
Vinicius Boscardin <stonervina@gmail.com>
2023-03-22 17:05:24 -0300

    mouseEvent

---
+++ js/modules/tooltip.js
@@ -0,0 +1,33 @@
+export default function initTooltip() {}
+
+const toolTips = document.querySelectorAll('[data-tooltip]');
+
+toolTips.forEach((item) => {
+  item.addEventListener('mouseover', onMouseOver);
+});
+
+function onMouseOver(event) {
+  const tooltipBox = criarTooltipBox(this);
+  tooltipBox.style.top = event.pageY + 'px';
+  tooltipBox.style.left = event.pageX + 'px';
+
+  onMouseLeave.tooltipBox = tooltipBox;
+  onMouseLeave.element = this;
+  this.addEventListener('mouseleave', onMouseLeave);
+}
+
+const onMouseLeave = {
+  handleEvent() {
+    this.tooltipBox.remove();
+    this.element.removeEventListener('mouseleave', onMouseLeave);
+  },
+};
+
+function criarTooltipBox(element) {
+  const tooltipBox = document.createElement('div');
+  const text = element.getAttribute('aria-label');
+  tooltipBox.classList.add('tooltip');
+  tooltipBox.innerText = text;
+  document.body.appendChild(tooltipBox);
+  return tooltipBox;
+}
---
+++ js/script.js
@@ -0,0 +1,15 @@
+import initScrollSuave from './modules/scroll-suave.js';
+import initAnimacaoScroll from './modules/scroll-animacao.js';
+import initAccordion from './modules/accordion.js';
+import initTabNav from './modules/tab-nav.js';
+import initModal from './modules/modal.js';
+import initCheck from './modules/checkbox-dark.js';
+import initTooltip from './modules/tooltip.js';
+
+initScrollSuave();
+initAnimacaoScroll();
+initAccordion();
+initTabNav();
+initModal();
+initCheck();
+initTooltip();
