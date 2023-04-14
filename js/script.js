01bc2cc5ad3ba2530696f13d2a5caf18b502a8f7
Vinicius Boscardin <stonervina@gmail.com>
2023-04-14 15:14:55 -0300

    fetch

--- js/script.js
+++ js/script.js
@@ -7,8 +7,8 @@ import initCheck from './modules/checkbox-dark.js';
 import initTooltip from './modules/tooltip.js';
 import initDropDownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
-import initAnimaNumeros from './modules/anima-numeros.js';
 import initFuncionamento from './modules/funcionamento.js';
+import initFetchAnimais from './modules/fetch-animais.js';
 
 initScrollSuave();
 initAnimacaoScroll();
@@ -19,5 +19,5 @@ initCheck();
 initTooltip();
 initDropDownMenu();
 initMenuMobile();
-initAnimaNumeros();
 initFuncionamento();
+initFetchAnimais();
