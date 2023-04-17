28f2f3bd89354a768136343000dcf7e8c245bc68
Vinicius Boscardin <stonervina@gmail.com>
2023-04-17 09:58:22 -0300

    btc-fetch

---
+++ js/modules/fetch-bitcoin.js
@@ -0,0 +1,5 @@
+export default function initFetchBitcoin() {
+
+}
+
+https://blockchain.info/ticker
--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ import initDropDownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
 import initFuncionamento from './modules/funcionamento.js';
 import initFetchAnimais from './modules/fetch-animais.js';
+import initFetchBitcoin from './modules/fetch-bitcoin.js';
 
 initScrollSuave();
 initAnimacaoScroll();
@@ -21,3 +22,4 @@ initDropDownMenu();
 initMenuMobile();
 initFuncionamento();
 initFetchAnimais();
+initFetchBitcoin();
